a44b50addc0cf38ca15fbd50bc4d4ca044f0dc1d
AbielRC08 <abielrc97@outlook.com>
2022-02-22 23:30:39 -0600

    Segunda Seccion

---
+++ lib/Seccion_uno.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:inversoft_v1/Screens/couter_screen.dart';
+import 'package:inversoft_v1/Screens/main_Screen.dart';
+
+void main(){
+  runApp(const Inversoft());
+}
+class Inversoft extends StatelessWidget {
+  //Constructor con key
+  const Inversoft({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:  Center(
+        child: CouterScreen(),
+      ),
+    );
+  }
+  
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:inversoft_v1/Screens/couter_screen.dart';
-import 'package:inversoft_v1/Screens/main_Screen.dart';
+import 'package:inversoft_v1/Screens/listview1_screen.dart';
 
-void main(){
-  runApp(const Inversoft());
-}
-class Inversoft extends StatelessWidget {
-  //Constructor con key
-  const Inversoft({Key? key}) : super(key: key);
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home:  Center(
-        child: CouterScreen(),
-      ),
+      title: 'Material App',
+      home: ListView1Screen()
     );
   }
-  
 }
