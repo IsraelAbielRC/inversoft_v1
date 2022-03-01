77e5339c0d1e903bd1668224450d8a2cdaf994c2
ABIELRC <abielrc97@outlook.com>
2022-02-28 20:36:39 -0600

    Mostrar Dialogo

--- lib/Screens/alert_screen.dart
+++ lib/Screens/alert_screen.dart
@@ -2,22 +2,50 @@ import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
+  void mostrarAlerta(BuildContext c) {
+    showDialog(
+        barrierDismissible: true,
+        context: c,
+        builder: (c) {
+          return AlertDialog(
+            elevation: 5,
+            title: const Text('Titulo'),
+            content: Column(mainAxisSize: MainAxisSize.min, children: const [
+              Text('Es un Contenido'),
+              FlutterLogo(
+                size: 100,
+              )
+            ]),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(c);
+                  },
+                  child: const Text('Cerrar'))
+            ],
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ElevatedButton(
-        child: const Padding(
-          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-          child: Text(
-            'Mostrar Alerta',
-            style: TextStyle(fontSize: 16),
+      body: Center(
+        child: ElevatedButton(
+          child: const Padding(
+            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+            child: Text(
+              'Mostrar Alerta',
+              style: TextStyle(fontSize: 16),
+            ),
           ),
+          onPressed: () => mostrarAlerta(context),
         ),
-        onPressed: () {},
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          Navigator.pop(context);
+        },
         child: const Icon(Icons.close_fullscreen_rounded),
       ),
     );
