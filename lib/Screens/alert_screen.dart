f23c7bde7960d5c46a9da8ef50a0e71dfb9848c5
ABIELRC <abielrc97@outlook.com>
2022-03-01 19:02:08 -0600

    Alert multi platarforma

--- lib/Screens/alert_screen.dart
+++ lib/Screens/alert_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
@@ -10,7 +13,8 @@ class AlertScreen extends StatelessWidget {
           return AlertDialog(
             elevation: 5,
             title: const Text('Titulo'),
-            shape: RoundedRectangleBorder(borderRadius: BorderRadiusDirectional.circular(10)),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadiusDirectional.circular(10)),
             content: Column(mainAxisSize: MainAxisSize.min, children: const [
               Text('Es un Contenido'),
               FlutterLogo(
@@ -28,20 +32,42 @@ class AlertScreen extends StatelessWidget {
         });
   }
 
+  void mostrarAlertaIOS(BuildContext c) {
+    showCupertinoDialog(
+        barrierDismissible: true,
+        context: c,
+        builder: (c) {
+          return CupertinoAlertDialog(
+            title: const Text('Titlo Cupertino'),
+            content: Column(mainAxisSize: MainAxisSize.min, children: const [
+              Text('Este es un contenido de Cupertino'),
+              SizedBox(height: 10),
+              FlutterLogo(size: 100)
+            ]),
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.pop(c),
+                  child: const Text('Cerrar'))
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-          child: const Padding(
-            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-            child: Text(
-              'Mostrar Alerta',
-              style: TextStyle(fontSize: 16),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+              child: Text(
+                'Mostrar Alerta',
+                style: TextStyle(fontSize: 16),
+              ),
             ),
-          ),
-          onPressed: () => mostrarAlerta(context),
-        ),
+            onPressed: () => Platform.isAndroid
+                ? mostrarAlerta(context)
+                : mostrarAlertaIOS(context)),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
